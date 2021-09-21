3f63877da06d00b8f06d2aff622fc3563c6ac46e
Rafael <raortega8906@gmail.com>
2021-09-22 01:54:25 +0200

    Update EmployeeFactory.php

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Company;
 use App\Models\Employee;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -21,7 +22,10 @@ class EmployeeFactory extends Factory
      */
     public function definition()
     {
+        $companies = collect(Company::all()->modelKeys());
+
         return [
+            'company_id' => $companies->random(),
             'name' => $this->faker->firstNameMale(),
             'last_name' => $this->faker->lastName(),
             'email' => $this->faker->freeEmail(),
