521e86b53746e27c662af8932ca56292bce262a6
Rafael <raortega8906@gmail.com>
2021-09-22 00:33:14 +0200

    Update Test with errors

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function loginAsAdmin()
+    {
+        $admin = User::factory()->create();
+        $this->actingAs($admin);
+
+        return $admin;
+    }
 }
--- tests/Unit/CompanyTest.php
+++ tests/Unit/CompanyTest.php
@@ -21,4 +21,47 @@ class CompanyTest extends TestCase
 
         $this->assertInstanceOf(Collection::class, $company->employees);
     }
+
+    /** @test */
+    public function user_can_create_company()
+    {
+        $user = $this->loginAsAdmin();
+        $this->assertTrue($user->can('create', new Company()));
+    }
+
+    /** @test */
+    public function user_can_view_company()
+    {
+        $user = $this->loginAsAdmin();
+        $company = Company::factory()->create([
+            'name' => 'Company 1 name',
+            'email' => 'email@emails.com',
+            'website' => 'http://example.com'
+        ]);
+        $this->assertTrue($user->can('view', $company));
+    }
+
+    /** @test */
+    public function user_can_update_company()
+    {
+        $user = $this->loginAsAdmin();
+        $company = Company::factory()->create([
+            'name' => 'Company 1 name',
+            'email' => 'email@emails.com',
+            'website' => 'http://example.com'
+        ]);
+        $this->assertTrue($user->can('update', $company));
+    }
+
+    /** @test */
+    public function user_can_delete_company()
+    {
+        $user = $this->loginAsAdmin();
+        $company = Company::factory()->create([
+            'name' => 'Company 1 name',
+            'email' => 'email@emails.com',
+            'website' => 'http://example.com'
+        ]);
+        $this->assertTrue($user->can('delete', $company));
+    }
 }
